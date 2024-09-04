0b565f75fd80a60266db841eac3c19f5f3b80009
ankit62018 <ankitkumar45870@gmail.in>
2024-09-04 16:43:02 +0530

    Create menu item home page completed

--- FoodDeleveryApp/frontend/src/pages/Home/Home.jsx
+++ FoodDeleveryApp/frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css' 
 import Header from '../../Compoents/Header/Header'
+import ExploreMenu from '../../Compoents/Exploremenu/ExploreMenu'
 
 const Home = () => {
+    const [category,setCategory] = useState("All")
   return (
     <div>
-      <Header></Header>
+      <Header/>
+      <ExploreMenu category = {category} setCategory = {setCategory}/>
     </div>
   )
 }
 
 export default Home
-    
